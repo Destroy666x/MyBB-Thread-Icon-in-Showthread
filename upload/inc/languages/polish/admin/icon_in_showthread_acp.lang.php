af709eb900ccc53257604b993a44149ce7716657
Przemek Pawlas <Destroy666@interia.pl>
2014-09-20 09:37:08 +0200

    Polish lang
    No idea why I'm translating 2 strings but ok...

---
+++ upload/inc/languages/polish/admin/icon_in_showthread_acp.lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Nazwa: Ikona tematu
+Autor: Destroy666
+Wersja: 1.0
+Informacje: Plugin dla skryptu MyBB, zakodowany dla wersji 1.8.x (może także działać w 1.6.x/1.4.x).
+Wyświetla wybraną ikonę tematu w showthread.php i na dwóch innych powiązanych stronach - printthread.php oraz newreply.php.
+3 zmiany w szablonach
+Licencja: GNU GPL v3, 29 June 2007. Więcej informacji w pliku LICENSE.md.
+Support: officjalne forum MyBB - http://community.mybb.com/mods.php?action=profile&uid=58253 (nie odpowiadam na PM, tylko na posty)
+Zgłaszanie błędów: mój github - https://github.com/Destroy666x
+
+© 2014 - date('Y')
+*/
+
+$l['icon_in_showthread'] = 'Ikona tematu';
+$l['icon_in_showthread_info'] = 'Wyświetla wybraną ikonę tematu w showthread.php i na dwóch innych powiązanych stronach - printthread.php oraz newreply.php.';
